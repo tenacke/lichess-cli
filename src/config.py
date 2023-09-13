c7a9a6b40a01ec977ed8d9f9026b05fc5a15c84e
tenacke <emrekilic1010@gmail.com>
2023-09-13 15:06:08 +0300

    help response in invalid entries fixed

--- src/config.py
+++ src/config.py
@@ -9,16 +9,16 @@ settings = None
 def check_valid_key(key):
     if '.' not in key:
         print('Invalid key\nYou should use the format section.option')
-        subprocess.call(['lichess', 'config', 'get', '--help'])
+        subprocess.call(['lichess', 'config', '--help'])
         sys.exit(1)
     section, option = key.split('.')
     if not settings.has_section(section):
         print('Invalid section:', section)
-        subprocess.call(['lichess', 'config', 'get', '--help'])
+        subprocess.call(['lichess', 'config', '--help'])
         sys.exit(1)
     if not settings.has_option(section, option):
         print('Invalid option:', option)
-        subprocess.call(['lichess', 'config', 'get', '--help'])
+        subprocess.call(['lichess', 'config', '--help'])
         sys.exit(1)
     
     return section, option
